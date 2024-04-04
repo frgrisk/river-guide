792a4e9b324eb06f5dca2a5ce8ea4f0409e2a0bf
Ze Loong <teohzeloong12@gmail.com>
2024-04-04 13:35:46 +0800

    doc: matched readme doc

--- cmd/root.go
+++ cmd/root.go
@@ -96,7 +96,7 @@ func init() {
 	rootCmd.Flags().String("favicon", "", "path to favicon")
 	rootCmd.Flags().Duration("read-header-timeout", defaultReadHeaderTimeout, "timeout for reading the request headers")
 	rootCmd.Flags().String("provider", "aws", "cloud provider (aws or azure)")
-	rootCmd.Flags().String("resource-group-name", "", "optional resource group name filter (valid only for Azure)")
+	rootCmd.Flags().String("resource-group-name", "", "filter instances based on their resource group membership (only used with the Azure provider)")
 	rootCmd.Flags().String("subscription-id", "", "subscription ID (valid only for Azure)")
 
 	err := viper.BindPFlags(rootCmd.Flags())
